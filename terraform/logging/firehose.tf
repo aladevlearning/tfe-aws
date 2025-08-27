553c3a244893b69293889f95be4a4913fa3a4f8b
ala <aladevlearning@gmail.com>
2025-08-27 14:35:36 +0200

    Add logging

--- terraform/logging/firehose.tf
+++ terraform/logging/firehose.tf
@@ -10,8 +10,6 @@ resource "aws_kinesis_firehose_delivery_stream" "cloudfront_realtime_logs" {
   extended_s3_configuration {
     role_arn           = aws_iam_role.firehose_delivery.arn
     bucket_arn         = aws_s3_bucket.logging.arn
-    buffering_interval = 60
-    buffering_size     = 5
     compression_format = "GZIP"
     error_output_prefix = "firehose/cloudfront/!{firehose:error-output-type}/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/"
     prefix              = "firehose/cloudfront/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/"
