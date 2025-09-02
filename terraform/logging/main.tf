35d99c20bcc0be5baf1b1ce4e652c256342785e2
ala <aladevlearning@gmail.com>
2025-09-02 14:23:30 +0200

    different approach

--- terraform/logging/main.tf
+++ terraform/logging/main.tf
@@ -11,7 +11,7 @@ data "aws_cloudfront_log_delivery_canonical_user_id" "cloudfront" {}
 module "waf_logs_bucket" {
   source       = "../modules/s3"
   bucket_name = "waf-logs"
-  cloudfront_arn = var.cloudfront_arn
+  aws_cloudwatch_log_delivery_source_name = var.aws_cloudwatch_log_delivery_source_name
   iam_role_arn = aws_iam_role.firehose_delivery.arn
   providers = {
     aws = aws
@@ -21,7 +21,7 @@ module "waf_logs_bucket" {
 module "access_logs_bucket" {
   source       = "../modules/s3"
   bucket_name = "access-logs"
-  cloudfront_arn = var.cloudfront_arn
+  aws_cloudwatch_log_delivery_source_name = var.aws_cloudwatch_log_delivery_source_name
   iam_role_arn = aws_iam_role.firehose_delivery.arn
   providers = {
     aws = aws
@@ -31,7 +31,7 @@ module "access_logs_bucket" {
 module "real_time_logs_bucket" {
   source       = "../modules/s3"
   bucket_name = "real-time-logs"
-  cloudfront_arn = var.cloudfront_arn
+  aws_cloudwatch_log_delivery_source_name = var.aws_cloudwatch_log_delivery_source_name
   iam_role_arn = aws_iam_role.firehose_delivery.arn
   providers = {
     aws = aws
--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -1,3 +1,3 @@
 variable "bucket_name" { type = string }
 variable "iam_role_arn" { type = string }
-variable "cloudfront_arn" { type = string }
+variable "aws_cloudwatch_log_delivery_source_name" { type = string }
