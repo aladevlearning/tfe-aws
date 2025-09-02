59e22055c0610d47575b0ad7c2977ad7563e615a
ala <aladevlearning@gmail.com>
2025-09-02 14:06:16 +0200

    different approach

--- terraform/modules/s3/cloudwatch.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "firehose" {
-  name              = "/aws/kinesisfirehose/cloudfront-realtime-logs-to-s3"
-  retention_in_days = 14
-}
-
-resource "aws_cloudwatch_log_stream" "firehose_logs_stream" {
-  name           = "S3Delivery"
-  log_group_name = aws_cloudwatch_log_group.firehose.name
-}
