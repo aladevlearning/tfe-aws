e0919707f8dba1aab4a328ae57cfbcbe2c63c03d
ala <aladevlearning@gmail.com>
2025-09-02 15:32:18 +0200

    different approach

--- terraform/modules/s3/s3.tf
+++ terraform/modules/s3/s3.tf
@@ -45,10 +45,6 @@ resource "aws_s3_bucket_acl" "example" {
       }
       permission = "READ_ACP"
     }
-
-    owner {
-      id = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
-    }
   }
 }
 
